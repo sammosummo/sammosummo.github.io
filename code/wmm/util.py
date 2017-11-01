10817b379a958d2e22d7ffc43051c3398f503f90
sammosummo <samuel.mathias@gmail.com>
2017-10-31 21:45:26 -0400

    changed logo

--- code/wmm/util.py
+++ code/wmm/util.py
@@ -4,6 +4,28 @@ import pandas as pd
 from collections import OrderedDict
 from itertools import product
 from patsy import dmatrix
+from patsy.contrasts import ContrastMatrix
+
+
+def _name_levels(prefix, levels):
+    return ["[%s%s]" % (prefix, level) for level in levels]
+
+
+class Simple(object):
+    def _simple_contrast(self, levels):
+        nlevels = len(levels)
+        contr = -1./nlevels * np.ones((nlevels, nlevels-1))
+        contr[1:][np.diag_indices(nlevels-1)] = (nlevels-1.)/nlevels
+        return contr
+
+    def code_with_intercept(self, levels):
+        contrast = np.column_stack((np.ones(len(levels)),
+                                    self._simple_contrast(levels)))
+        return ContrastMatrix(contrast, _name_levels("Simp.", levels))
+
+    def code_without_intercept(self, levels):
+        contrast = self._simple_contrast(levels)
+        return ContrastMatrix(contrast, _name_levels("Simp.", levels[:-1]))
 
 
 def get_rouder_data():
