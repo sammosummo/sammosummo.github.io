9587a76330170dc105af5d8490bdf8ba0f545fef
samuelrobertmathias <samuel.mathias@gmail.com>
2019-05-26 20:37:00 -0400

    figures

--- _scripts/sdt_yn_fig.py
+++ _scripts/sdt_yn_fig.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     figsize = defaults["figure.figsize"]
     defaults["figure.figsize"] = [figsize[0], int(figsize[1] * 2 / 3)]
-    defaults["lines.linewidth"] = 2
+    defaults["lines.linewidth"] = 3
     defaults["font.size"] = 14
 
     d = 1
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     second = norm.pdf(psi, d, 1)
 
     fig, ax = plt.subplots(1, 1, constrained_layout=True)
-    ax.plot(psi, first, label=r"$x=0$", linewidth=3)
-    ax.plot(psi, second, label=r"$x=1$", linewidth=3)
+    ax.plot(psi, first, label=r"$X=0$")
+    ax.plot(psi, second, label=r"$X=1$")
     ax.set_xlim(-3, 3 + d)
     ax.set_ylim(0, first.max() * 1.05)
     ax.set_xticks([0, d])
@@ -35,3 +35,49 @@ if __name__ == '__main__':
     f = f"../assets/images/evg_yn_perceptual.svg"
     plt.legend()
     plt.savefig(f, bbox_inches=0, transparent=True)
+
+    k = 0.3
+    line = plt.axvline(k, c="black")
+    ax.set_xticks([0, k, d])
+    ax.set_xticklabels([0, r"$k$", r"$d$"], fontdict={"fontsize": 12})
+    f = f"../assets/images/evg_yn_response.svg"
+    plt.savefig(f, bbox_inches=0, transparent=True)
+
+    a = np.where(psi.round(3) == k)[0][0]
+    fill = plt.fill_between(psi[a:], first[a:], alpha=0.3)
+    f = f"../assets/images/evg_yn_fa.svg"
+    plt.savefig(f, bbox_inches=0, transparent=True)
+
+    fill.set_visible(False)
+    fill = plt.fill_between(psi[a:], second[a:], alpha=0.3)
+    f = f"../assets/images/evg_yn_hit.svg"
+    plt.savefig(f, bbox_inches=0, transparent=True)
+
+    fill.set_visible(False)
+    line.set_visible(False)
+    kun = d / 2
+    line = plt.axvline(kun, c="black")
+    a = np.where(psi.round(3) == kun)[0][0]
+    fill0 = plt.fill_between(psi[a:], second[a:], alpha=0.3, color="C1")
+    fill1 = plt.fill_between(psi[:a], first[:a], alpha=0.3, color="C2")
+    ax.set_xticks([0, kun, d])
+    ax.set_xticklabels([0, r"$k_\mathrm{unb}$", r"$d$"], fontdict={"fontsize": 12})
+    f = f"../assets/images/evg_yn_unbiased.svg"
+    plt.savefig(f, bbox_inches=0, transparent=True)
+
+    k = 0.2
+    fill0.set_visible(False)
+    fill1.set_visible(False)
+    line.set_visible(False)
+    ax.set_xticks([0, k, kun, d])
+    ax.set_xticklabels(
+        [0, r"$k$", r"$k_\mathrm{unb}$", r"$d$"], fontdict={"fontsize": 12}
+    )
+    ax.annotate("", (k, 0.3), (kun, 0.3), arrowprops={"arrowstyle": "|-|"})
+    plt.text((k+kun)/2.5, 0.22, "$c$")
+    f = f"../assets/images/evg_yn_criterion.svg"
+    plt.savefig(f, bbox_inches=0, transparent=True)
+
+
+
+
