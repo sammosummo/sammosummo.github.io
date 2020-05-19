9bc18818bb27ef3588bedb34f94073cd5bf62fe2
samuelrobertmathias <samuel.mathias@gmail.com>
2020-05-19 10:34:00 -0400

    new post

--- assets/scripts/bcfa-long.py
+++ assets/scripts/bcfa-long.py
@@ -174,23 +174,17 @@ def bcfau(items, factors, paths, nu_sd=2.5, alpha_sd=2.5, d_beta=2.5):
     pm.Normal(name="$Y$", mu=M, sigma=S, observed=items, shape=items.shape)
 
 
-def bcfabin(
-    items, factors, nu_sd=2.5, alpha_sd=2.5, d_beta=2.5,
-):
-    r"""Constructs a Bayesian CFA model in "univariate form" by directly estimating the
-    factors, with a binomial likelihood.
+def bcfab(items, factors, paths, nu_sd=2.5, alpha_sd=2.5):
+    r"""Constructs a Bayesian CFA model in "binomial form".
 
     Args:
-        items (np.array): Array of item data.
+        items (np.array): Data.
         factors (np.array): Factor design matrix.
-        beta (:obj:`float` or `'estimate'`, optional): Standard deviation of normal
-            prior on cross loadings. If `'estimate'`,  beta is estimated from the data.
+        paths (np.array): Paths design matrix.
         nu_sd (:obj:`float`, optional): Standard deviation of normal prior on item
             intercepts.
         alpha_sd (:obj:`float`, optional): Standard deviation of normal prior on factor
             intercepts.
-        d_beta (:obj:`float`, optional): Scale parameter of half-Cauchy prior on factor
-            standard deviation.
 
     Returns:
         None: Places model in context.
@@ -201,17 +195,15 @@ def bcfabin(
     p_, m = factors.shape
     assert p == p_, "Mismatch between data and factor-loading matrices"
 
-    binom_n = np.zeros_like(items) + items.max(axis=1).til
-
     # priors on item intercepts
-    nu = pm.Normal(name=r"$\nu$", mu=0, sd=nu_sd, shape=p, testval=items.mean(axis=0))
+    nu = pm.Normal(name=r"$\nu$", mu=0, sd=nu_sd, shape=p, testval=np.zeros(p))
 
     # priors on factor intercepts
     alpha = pm.Normal(name=r"$\alpha$", mu=0, sd=alpha_sd, shape=m, testval=np.zeros(m))
 
     # priors on factor loadings
     l = np.asarray(factors).sum()
-    lam = pm.Normal(name=r"$\lambda$", mu=0, sd=1, shape=l, testval=np.ones(l))
+    lam = pm.Normal(name=r"$\lambda$", mu=0, sd=1, shape=l, testval=np.zeros(l))
 
     # loading matrix
     Lambda = tt.zeros(factors.shape)
@@ -222,22 +214,30 @@ def bcfabin(
             k += 1
     pm.Deterministic(name=r"$\Lambda$", var=Lambda)
 
-    # starting values for factors
-    st = np.dot(items, factors) / factors.sum(axis=0)
+    # priors on paths
+    g = np.asarray(paths).sum()
+    gam = pm.Normal(name=r"$\gamma$", mu=0, sd=1, shape=g, testval=np.zeros(g))
 
-    # priors on factors
-    eta = pm.Normal(name=r"$\eta$", mu=0, sigma=1, shape=(n, m), testval=st)
+    # path matrix
+    Gamma = tt.zeros(paths.shape)
+    k = 0
+    for i, j in product(range(m), range(m)):
+        if paths[i, j] == 1:
+            Gamma = tt.inc_subtensor(Gamma[i, j], gam[k])
+            k += 1
+    pm.Deterministic(name=r"$\Gamma$", var=Gamma)
 
-    # item means
-    p = pm.math.sigmoid(nu + matrix_dot(alpha + eta, Lambda.T))
+    # priors on factor residuals
+    zeta = pm.Normal(name=r"$\zeta$", mu=0, sigma=1, shape=(n, m), testval=0)
 
-    # # item residual standard deviations
-    # d = pm.HalfCauchy(
-    #     name=r"$\sqrt{\theta}$", beta=d_beta, shape=p, testval=items.std(axis=0)
-    # )
+    # latent variables
+    I = np.eye(m)
+    Pi = pm.math.sigmoid(nu + matrix_dot(
+        matrix_dot((alpha + zeta), matrix_inverse(I - Gamma.T)), Lambda.T
+    ))
 
     # observations
-    pm.Binomial(name="$Y$", p=p, n=binom_n, observed=items, shape=items.shape)
+    pm.Binomial(name="$Y$", p=Pi, n=items.max(axis=0), observed=items, shape=items.shape)
 
 
 def main():
@@ -308,23 +308,23 @@ def main():
     for school, sdf in df.groupby("school"):
 
         # define the path to save results
-        f = f"../data/BSEM (long) examples/{school} (uni)"
+        f = f"../data/BSEM (long) examples/{school} (bin)"
 
         # select the 19 commonly used variables
         items = sdf[item_names]
 
         # for numerical convenience, standardize the data
-        items = (items - items.mean()) / items.std()
+        # items = (items - items.mean()) / items.std()
 
         with pm.Model():
 
             # construct the model
-            bcfau(items, factors, paths)
+            bcfab(items, factors, paths)
 
             if not exists(f):
 
                 # sample and save
-                trace = pm.sample(10000, tune=1000, chains=1)
+                trace = pm.sample(5000, tune=1000, chains=1)
                 pm.save_trace(trace, f)
                 pm.traceplot(trace, compact=True)
                 rcParams["font.size"] = 14
